3d4f4ef380f0a1051d3fb1af04bcd2db0087c702
srfmolina <srfmolina+git@pm.me>
2026-04-01 20:23:04 +0200

    feat: Minor improvements

--- composeApp/src/commonMain/kotlin/com/srfmolina/krocy/App.kt
+++ composeApp/src/commonMain/kotlin/com/srfmolina/krocy/App.kt
@@ -9,15 +9,6 @@ import androidx.compose.ui.Modifier
 import com.srfmolina.krocy.presentation.navigation.NavigationComponent
 import com.srfmolina.krocy.presentation.theme.KrocyTheme
 
-
-//fun todaysDate(): String {
-//    fun LocalDateTime.format() = toString().substringBefore('T')
-//
-//    val now = Clock.System.now()
-//    val zone = TimeZone.currentSystemDefault()
-//    return now.toLocalDateTime(zone).format()
-//}
-
 @Composable
 fun App() {
     KrocyTheme {
@@ -40,34 +31,3 @@ fun App() {
         }
     }
 }
-
-/*
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = "Today's date is ${todaysDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
-            )
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { "Hola" }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
- */
